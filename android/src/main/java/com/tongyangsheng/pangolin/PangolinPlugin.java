bcdd9e97bb8683a73660c94a6ae91a3a3bd9ae69
rowger <php_echo@163.com>
2020-04-27 18:11:15 +0800

    接入原生TextView

--- android/src/main/java/com/tongyangsheng/pangolin/PangolinPlugin.java
+++ android/src/main/java/com/tongyangsheng/pangolin/PangolinPlugin.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 
 import androidx.annotation.NonNull;
 
+import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.embedding.engine.plugins.activity.ActivityAware;
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
@@ -17,26 +18,29 @@ import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 
-/** PangolinPlugin */
+/**
+ * PangolinPlugin
+ */
 public class PangolinPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware {
     private MethodChannel methodChannel;
     private Context applicationContext;
     private Activity activity;
 
 
-  @Override
-  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-    BinaryMessenger messenger = flutterPluginBinding.getBinaryMessenger();
-    flutterPluginBinding
-            .getFlutterEngine()
-            .getPlatformViewsController()
-            .getRegistry()
-            .registerViewFactory(
-                    "feedadview", new FeedAdFactory(messenger, /*containerView=*/ null));
-      onAttachedToEngine(flutterPluginBinding.getApplicationContext(), flutterPluginBinding.getBinaryMessenger());
-  }
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+        BinaryMessenger messenger = flutterPluginBinding.getBinaryMessenger();
+        flutterPluginBinding.getPlatformViewRegistry().registerViewFactory("feedadview", new FeedAdFactory(messenger, null));
 
-    private void onAttachedToEngine(Context applicationContext , BinaryMessenger messenger) {
+//        flutterPluginBinding
+//                .getFlutterEngine()
+//                .getPlatformViewsController()
+//                .getRegistry()
+//                .registerViewFactory("feedadview", new FeedAdFactory(messenger, null));
+        onAttachedToEngine(flutterPluginBinding.getApplicationContext(), flutterPluginBinding.getBinaryMessenger());
+    }
+
+    private void onAttachedToEngine(Context applicationContext, BinaryMessenger messenger) {
         this.applicationContext = applicationContext;
         methodChannel = new MethodChannel(messenger, "com.tongyangsheng.pangolin");
         methodChannel.setMethodCallHandler(this);
@@ -66,138 +70,116 @@ public class PangolinPlugin implements FlutterPlugin, MethodCallHandler, Activit
     public void onDetachedFromActivity() {
 
     }
-  // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-  // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-  // plugin registration via this function while apps migrate to use the new Android APIs
-  // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-  //
-  // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-  // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-  // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-  // in the same class.
-  public static void registerWith(Registrar registrar) {
+
+    // This static function is optional and equivalent to onAttachedToEngine. It supports the old
+    // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
+    // plugin registration via this function while apps migrate to use the new Android APIs
+    // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
+    //
+    // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
+    // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
+    // depending on the user's project. onAttachedToEngine or registerWith must both be defined
+    // in the same class.
+    public static void registerWith(Registrar registrar) {
 
 
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "com.tongyangsheng.pangolin");
-    channel.setMethodCallHandler(new PangolinPlugin());
-    // 注册
-    // registrar.platformViewRegistry().registerViewFactory("feedadview",new AdFactory(registrar));
+        final MethodChannel channel = new MethodChannel(registrar.messenger(), "com.tongyangsheng.pangolin");
+        channel.setMethodCallHandler(new PangolinPlugin());
+        // 注册
+        // registrar.platformViewRegistry().registerViewFactory("feedadview",new AdFactory(registrar));
 
-  }
-
-  @Override
-  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-    if (call.method.equals("register")) {
-      String appId = call.argument("appId");
-      Boolean useTextureView = call.argument("useTextureView");
-      String appName = call.argument("appName");
-      Boolean allowShowNotify = call.argument("allowShowNotify");
-      Boolean allowShowPageWhenScreenLock = call.argument("allowShowPageWhenScreenLock");
-      Boolean debug = call.argument("debug");
-      Boolean supportMultiProcess = call.argument("supportMultiProcess");
-      if(useTextureView == null)
-      {
-        useTextureView = false;
-      }
-      if (allowShowNotify == null)
-      {
-        allowShowNotify = true;
-      }
-      if (allowShowPageWhenScreenLock == null)
-      {
-        allowShowPageWhenScreenLock = true;
-      }
-      if (debug == null)
-      {
-        debug = true;
-      }
-      if (supportMultiProcess == null)
-      {
-        supportMultiProcess = false;
-      }
-      if (appId == null || appId == "")
-      {
-        result.error("500","appId can't be null",null);
-      }
-      else {
-        if (appName == null || appName == "") {
-          result.error("600", "appName can't be null", null);
-        } else {
-          TTAdManagerHolder.init(applicationContext, appId, useTextureView, appName, allowShowNotify, allowShowPageWhenScreenLock, debug, supportMultiProcess);
-          result.success(true);
-        }
-      }
     }
-    else if(call.method.equals("loadSplashAd"))
-    {
-      String mCodeId = call.argument("mCodeId");
-      Boolean deBug = call.argument("debug");
-      Intent intent = new Intent();
-      intent.setClass(activity,SplashActivity.class);
-      intent.putExtra("mCodeId",mCodeId);
-      intent.putExtra("debug",deBug);
-      activity.startActivity(intent);
-    }
-    else if (call.method.equals("loadRewardAd"))
-    {
-      Boolean isHorizontal = call.argument("isHorizontal");
-      String mCodeId = call.argument("mCodeId");
-      Boolean debug = call.argument("debug");
-      Boolean supportDeepLink = call.argument("supportDeepLink");
-      String rewardName = call.argument("rewardName");
-      int rewardAmount = (int)call.argument("rewardAmount");
-      Boolean isExpress = call.argument("isExpress");
 
-      double expressViewAcceptedSizeH;
-      if (call.argument("expressViewAcceptedSizeH") == null)
-      {
-        expressViewAcceptedSizeH = 500;
-      }
-      else
-      {
-        expressViewAcceptedSizeH = call.argument("expressViewAcceptedSizeH");
-      }
-      double expressViewAcceptedSizeW;
-      if (call.argument("expressViewAcceptedSizeW") == null)
-      {
-        expressViewAcceptedSizeW = 500;
-      }
-      else
-      {
-        expressViewAcceptedSizeW = call.argument("expressViewAcceptedSizeW");
-      }
+    @Override
+    public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+        if (call.method.equals("register")) {
+            String appId = call.argument("appId");
+            Boolean useTextureView = call.argument("useTextureView");
+            String appName = call.argument("appName");
+            Boolean allowShowNotify = call.argument("allowShowNotify");
+            Boolean allowShowPageWhenScreenLock = call.argument("allowShowPageWhenScreenLock");
+            Boolean debug = call.argument("debug");
+            Boolean supportMultiProcess = call.argument("supportMultiProcess");
+            if (useTextureView == null) {
+                useTextureView = false;
+            }
+            if (allowShowNotify == null) {
+                allowShowNotify = true;
+            }
+            if (allowShowPageWhenScreenLock == null) {
+                allowShowPageWhenScreenLock = true;
+            }
+            if (debug == null) {
+                debug = true;
+            }
+            if (supportMultiProcess == null) {
+                supportMultiProcess = false;
+            }
+            if (appId == null || appId == "") {
+                result.error("500", "appId can't be null", null);
+            } else {
+                if (appName == null || appName == "") {
+                    result.error("600", "appName can't be null", null);
+                } else {
+                    TTAdManagerHolder.init(applicationContext, appId, useTextureView, appName, allowShowNotify, allowShowPageWhenScreenLock, debug, supportMultiProcess);
+                    result.success(true);
+                }
+            }
+        } else if (call.method.equals("loadSplashAd")) {
+            String mCodeId = call.argument("mCodeId");
+            Boolean deBug = call.argument("debug");
+            Intent intent = new Intent();
+            intent.setClass(activity, SplashActivity.class);
+            intent.putExtra("mCodeId", mCodeId);
+            intent.putExtra("debug", deBug);
+            activity.startActivity(intent);
+        } else if (call.method.equals("loadRewardAd")) {
+            Boolean isHorizontal = call.argument("isHorizontal");
+            String mCodeId = call.argument("mCodeId");
+            Boolean debug = call.argument("debug");
+            Boolean supportDeepLink = call.argument("supportDeepLink");
+            String rewardName = call.argument("rewardName");
+            int rewardAmount = (int) call.argument("rewardAmount");
+            Boolean isExpress = call.argument("isExpress");
 
-      String userID = call.argument("userID");
-      String mediaExtra;
-      if (call.argument("mediaExtra") == null)
-      {
-        mediaExtra = "media_extra";
-      }
-      else
-      {
-        mediaExtra = call.argument("mediaExtra");
-      }
+            double expressViewAcceptedSizeH;
+            if (call.argument("expressViewAcceptedSizeH") == null) {
+                expressViewAcceptedSizeH = 500;
+            } else {
+                expressViewAcceptedSizeH = call.argument("expressViewAcceptedSizeH");
+            }
+            double expressViewAcceptedSizeW;
+            if (call.argument("expressViewAcceptedSizeW") == null) {
+                expressViewAcceptedSizeW = 500;
+            } else {
+                expressViewAcceptedSizeW = call.argument("expressViewAcceptedSizeW");
+            }
 
-      RewardVideo rewardVideo = new RewardVideo();
-      RewardVideo._channel = methodChannel;
-      rewardVideo.activity = activity;
-      rewardVideo.context = applicationContext;
-      if (isHorizontal)
-      {
-        rewardVideo.mHorizontalCodeId = mCodeId;
-      }
-      else
-      {
-        rewardVideo.mVerticalCodeId = mCodeId;
-      }
-      rewardVideo.supportDeepLink = supportDeepLink;
-      rewardVideo.expressViewAcceptedSizeH = expressViewAcceptedSizeH;
-      rewardVideo.expressViewAcceptedSizeW = expressViewAcceptedSizeW;
-      rewardVideo.rewardName = rewardName;
-      rewardVideo.rewardAmount = rewardAmount;
-      rewardVideo.userID = userID;
-      rewardVideo.mediaExtra = mediaExtra;
-      rewardVideo.init();
+            String userID = call.argument("userID");
+            String mediaExtra;
+            if (call.argument("mediaExtra") == null) {
+                mediaExtra = "media_extra";
+            } else {
+                mediaExtra = call.argument("mediaExtra");
+            }
+
+            RewardVideo rewardVideo = new RewardVideo();
+            RewardVideo._channel = methodChannel;
+            rewardVideo.activity = activity;
+            rewardVideo.context = applicationContext;
+            if (isHorizontal) {
+                rewardVideo.mHorizontalCodeId = mCodeId;
+            } else {
+                rewardVideo.mVerticalCodeId = mCodeId;
+            }
+            rewardVideo.supportDeepLink = supportDeepLink;
+            rewardVideo.expressViewAcceptedSizeH = expressViewAcceptedSizeH;
+            rewardVideo.expressViewAcceptedSizeW = expressViewAcceptedSizeW;
+            rewardVideo.rewardName = rewardName;
+            rewardVideo.rewardAmount = rewardAmount;
+            rewardVideo.userID = userID;
+            rewardVideo.mediaExtra = mediaExtra;
+            rewardVideo.init();
 
 //      if (isHorizontal == null)
 //      {
@@ -238,12 +220,10 @@ public class PangolinPlugin implements FlutterPlugin, MethodCallHandler, Activit
 //          activity.startActivity(intent);
 //        }
 //      }
+        } else if (call.method.equals("loadFeedAd ")) {
+            String mCodeId = call.argument("mCodeId");
+        } else {
+            result.notImplemented();
+        }
     }
-    else if(call.method.equals("loadFeedAd ")){
-      String mCodeId = call.argument("mCodeId");
-    } else
-      {
-      result.notImplemented();
-    }
-  }
 }
--- lib/FeedAdView.dart
+++ lib/FeedAdView.dart
@@ -3,17 +3,15 @@ import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 
-//View创建完成回调
-typedef void WebViewCreatedCallback(WebController controller);
+
 
 //View上层组件
 class FeedAdView extends StatefulWidget {
 
-  final WebViewCreatedCallback onWebCreated;
+
 
   FeedAdView({
     Key key,
-    @required this.onWebCreated,
   });
 
   @override
@@ -29,14 +27,10 @@ class _FeedAdViewState extends State<FeedAdView>{
     if(defaultTargetPlatform == TargetPlatform.android){
       return AndroidView(
         viewType: 'feedadview',
-        onPlatformViewCreated: onPlatformViewCreated,
-        creationParamsCodec: const StandardMessageCodec(),
       );
     } else if(defaultTargetPlatform == TargetPlatform.iOS){
       return UiKitView(
         viewType: 'feedadview',
-        onPlatformViewCreated: onPlatformViewCreated,
-        creationParamsCodec: const StandardMessageCodec(),
       );
     }
 
@@ -46,29 +40,8 @@ class _FeedAdViewState extends State<FeedAdView>{
 
   //视图创建完成
   Future<void> onPlatformViewCreated(id) async{
-    if(widget.onWebCreated == null){
-      return;
-    }
-    widget.onWebCreated(WebController.init(id));
-
-  }
-
-}
 
 
-//Web控制类
-class WebController {
-
-  MethodChannel _channel;
-
-  WebController.init(int id){
-    _channel = MethodChannel('feedadview_$id');
-  }
-
-  //提供加载URL方法
-  Future<void> loadUrl(String url) async{
-    assert(url != null);
-    return _channel.invokeMethod('loadUrl',url);
   }
 
 }
